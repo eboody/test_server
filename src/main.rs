47a940b348c0381b85e9446492f4d622cfcfcba6
Eran Boodnero <eboodnero@gmail.com>
2024-06-26 11:33:48 -0700

    First

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use axum::{response::Html, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { Html("<div>Hello world</div>") }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
